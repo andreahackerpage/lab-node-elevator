06eeae54618bedd3c9a90825e6ae01e9e5af34ac
Andrea Hacker <andreamrios@MacBook-Pro-de-Andrea.local>
2018-02-13 08:54:40 -0600

    semidone

--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,14 @@
 const Elevator = require('./elevator.js');
+
+const Person= require('./person.js');
+
+let myElevator = new Elevator ();
+const per1= new Person ("Andrea", 4, 6);
+const per2= new Person ("Ky", 7, 1);
+
+myElevator.call(per1);
+myElevator.call(per2)
+
+
+console.log(myElevator.waitingList)
+myElevator.start();
